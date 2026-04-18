c56246afb51d000cad0b8799da09007247bbde0d
ganesh <ganesh@GANESHKRISHNA>
2026-04-18 12:33:05 +0530

    day1 sum

---
+++ Day1/sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Prompt the user to enter the first number
+        Console.WriteLine("Enter the first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        // Complete Step 1:............
+
+        // Prompt the user to enter the second number
+        Console.WriteLine("Enter the second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int sum = num1 + num2;
+        Console.WriteLine("The sum is: " + sum);
+        // Complete Step 2:............
+
+        // Add the two numbers
+        // Complete Step 3:............
+
+        // Print the sum
+        // Complete Step 4:............
+    }
+}
