d355e9f56acc2103d0992152a5146e5c2528cae7
ganesh <ganesh@GANESHKRISHNA>
2026-04-22 10:13:07 +0530

    day5Eval1

---
+++ day5/Day5Eval1/Day5Eval1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Book
+{
+    public string Title;
+    public string Author;
+    public string ISBN;
+
+    public Book(string title, string author, string isbn)
+    {
+        Title = title;
+        Author = author;
+        ISBN = isbn;
+    }
+}
+
+class Library
+{
+    List<Book> books = new List<Book>();
+
+    public void Add(Book book)
+    {
+        books.Add(book);
+        Console.WriteLine("Book added successfully.");
+    }
+
+    public void Remove(string isbn)
+    {
+        int removed = books.RemoveAll(b => b.ISBN == isbn);
+
+        if (removed > 0)
+            Console.WriteLine("Book removed successfully.");
+        else
+            Console.WriteLine("Book not found.");
+    }
+
+    public void ListBooks()
+    {
+        foreach (Book b in books)
+        {
+            Console.WriteLine($"Title: {b.Title}, Author: {b.Author}, ISBN: {b.ISBN}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Library library = new Library();
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Add Book");
+            Console.WriteLine("2. Remove Book");
+            Console.WriteLine("3. List Books");
+            Console.WriteLine("4. Exit");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter book title:");
+                    string title = Console.ReadLine();
+
+                    Console.WriteLine("Enter book author:");
+                    string author = Console.ReadLine();
+
+                    Console.WriteLine("Enter book ISBN:");
+                    string isbn = Console.ReadLine();
+
+                    Book newBook = new Book(title, author, isbn);
+                    library.Add(newBook);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter book ISBN to remove:");
+                    string removeIsbn = Console.ReadLine();
+                    library.Remove(removeIsbn);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Listing all books:");
+                    library.ListBooks();
+                    break;
+
+                case 4:
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
